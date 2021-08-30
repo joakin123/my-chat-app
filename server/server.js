7edf3c42f133a7efc2d2f1e66f73d1b67177303f
joakin <akinbodejoshua145@gmail.com>
2021-08-30 19:24:01 +0100

    Testing the depoly app\

--- server/server.js
+++ server/server.js
@@ -6,28 +6,28 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname + '/../public');
 var port = process.env.PORT || 3000;
 const app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
+// var server = http.createServer(app);
+// var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
-    console.log('New user connected');
+// io.on('connection', (socket) => {
+//     console.log('New user connected');
 
-    socket.on('createMessage', (message) => {
-        console.log('New Message:', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
-    });
+//     socket.on('createMessage', (message) => {
+//         console.log('New Message:', message);
+//         io.emit('newMessage', {
+//             from: message.from,
+//             text: message.text,
+//             createdAt: new Date().getTime()
+//         });
+//     });
 
-    socket.on('disconnect', () => {
-        console.log('user was disconnected');
-    })
-});
+//     socket.on('disconnect', () => {
+//         console.log('user was disconnected');
+//     })
+// });
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 })
